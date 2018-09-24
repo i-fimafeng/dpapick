695e444254be91184d969fcbb0ed46429e54ac27
Ichigo Fimageng <ichigo.fimafeng@gmail.com>
2018-09-23 21:16:42 -0400

    remove unnecessary dependency source directory (handled by setup.py and pip)

--- depends/CFPropertyList/tests/test_dictionary.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from reference import parsed_binary
-
-class DictionaryTest(unittest.TestCase):
-    def test_read_dictionary(self):
-        self.assertEqual({'key':'value'}, parsed_binary('dictionary'))    
-
-if __name__ == '__main__':
-    unittest.main()
